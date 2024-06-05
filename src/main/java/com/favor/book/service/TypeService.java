f05705ecda71c9c9541f9f50daafb0c40808d4fb
CQ <2227558856@qq.com>
2024-06-05 20:30:13 +0800

    fix(书籍类型): 类型名称不允许为空

--- src/main/java/com/favor/book/service/TypeService.java
+++ src/main/java/com/favor/book/service/TypeService.java
@@ -37,6 +37,21 @@ public class TypeService {
     }
 
     public Result addType(Type type) {
+        /*
+        isEmpty: 判断对象是否为空。
+        对于字符串：
+            如果字符串的长度为 0，则返回 true。
+            如果字符串不为 null 且包含除空格外的字符，则返回 false。
+        对于集合（如 List、Set 等）：
+            如果集合中不包含任何元素，则返回 true。
+            如果集合不为 null 且包含元素，则返回 false。
+        对于数组：
+            如果数组的长度为 0，则返回 true。
+            如果数组不为 null 且包含元素，则返回 false。
+         */
+        if (type.getName().isEmpty()) {
+            return Result.error("类型名称不能为空");
+        }
         if (typeRepository.findFirstByName(type.getName()).isPresent()) {
             return Result.error("类型已存在");
         }
